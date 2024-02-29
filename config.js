1501494ee3d400b4a1659e955b0bf54ec0c5af94
Qeme_AX2 <qemeofficial07@gmail.com>
2024-02-29 16:37:05 +0800

    Login/Logout v0.1, Roles/Tournament push v0.1

---
+++ config.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+const connect = mongoose.connect("mongodb://localhost:27017/esms")
+
+//check the connection if is it connected or not
+connect.then(()=>{
+    console.log("Database is connected succesfully")
+}).catch(()=>{
+    console.log("Database is failed to connect")
+})
+
+//create a Schema
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: [String], // Array of strings
+        default: ['normal-user'] // Default value
+    },
+    tournaments: {
+        type: [String],
+        default: [null]
+    }
+})
+
+//create the Collection part
+const collection = new mongoose.model("users",LoginSchema)
+
+//export the module to be used inside server.js
+module.exports = collection
---
+++ server.js
@@ -0,0 +1,180 @@
+//only for process env not for production env
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+//extract the modules
+const express = require("express")
+const app = express()
+const bcrypt = require("bcrypt")
+const passport = require("passport")
+const flash = require("express-flash")
+const session = require("express-session")
+const methodOverride = require("method-override")
+
+const initializePassport = require('./passport-config')
+const collection = require('./config')
+
+//pass the passport modules to other function at other file
+initializePassport(
+    passport,
+    async email => await collection.findOne({ email: email }), // Asynchronously fetch user by email
+    async id => await collection.findById(id) // Asynchronously fetch user by ID
+)
+
+//render engine
+app.set('view engine', 'ejs');
+
+//adding middleware to the Express application. 
+//Middleware functions are functions that have access to the request object (req)
+//plus the response object (res), and the next middleware function in the application's request-response cycle
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    //this will take the information from .env file
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+//transfer the visitors to the main page
+//when no argument inserted after /
+// app.get('/', checkAuthenticated, (req,res)=>{
+//     res.render('index.ejs', {name: req.user.name}) //take loged in user's name
+// })
+
+
+//need to use async funct because it takes time to grab the data from mongodb
+app.get('/',async (req, res) => {
+    // console.log(await req.user)
+    res.render('index', { user: await req.user });
+});
+
+
+
+//for /login argument 
+app.get('/login', checkNotAuthenticated, (req,res)=>{
+    res.render('login.ejs')
+})
+
+//POST /login why now we just use the passport module instead of req res
+app.post('/login', checkNotAuthenticated, passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+    //to do the failureFlash message, u need to use ejs at login.ejs
+}))
+
+//for /register argument
+app.get('/register', checkNotAuthenticated, (req,res)=>{
+    //define error type
+    res.render('register.ejs', { error: req.query.error })
+})
+
+//POST /register
+app.post('/register', checkNotAuthenticated, async (req,res)=>{
+    try{
+
+        // Check if the email already exists in the database
+        const existingUser = await collection.findOne({ email: req.body.email });
+        
+        if(existingUser) {
+            // If the email already exists, redirect the user to the registration page with an error message
+            // if yes, send error = email-exists type
+            return res.redirect('/register?error=email-exists');
+        }
+        
+        //hash the password first
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+
+        //then we push the inserted data to database
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        }
+
+        //transfer all the data to the database
+        const userdata = await collection.insertMany([data])
+
+        //after all settle, redirect user to login page
+        res.redirect('/login')
+    }catch{
+        //if have any error, let's redirect user to register again
+        res.redirect('/register')
+    }
+})
+
+// Route for updating user roles as competitor
+app.post('/update-roles', checkAuthenticated,async (req, res) => {
+    try {
+
+        //get the details of the registered user
+        const user = await req.user
+
+        console.log(user)
+        console.log(req.body.roles)
+
+        if (!user) {
+            res.status(404).json({ message: 'User not found' })
+        }
+
+        //get the JSON data and put it to specific attributes
+        user.roles.push(req.body.roles)
+        user.tournaments.push(req.body.tournaments)
+
+        //save the push value to the mongodb
+        user.save()
+
+        res.redirect('/')
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+//this logout gave error because req.logout is asynchronous
+//where u will get "req#logout requires a callback function"
+// app.delete('/logout',(req,res)=>{
+//     req.logOut()
+//     res.redirect('/login')
+// })
+//use below instead
+app.delete('/logout', (req, res, next) => {
+    req.logOut((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/login');
+    });
+  });
+
+//create a function to check Authentication user as middleware
+//this function will be executed first inside '/' so if the 
+//user is not loged in, redirect user to login
+//this can avoid user from entering the page without permission
+function checkAuthenticated(req,res,next){
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+//this function is to avoid from the loged in user from auto log out 
+//when the user type http://.../login 
+function checkNotAuthenticated(req,res,next){
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+
+    next()
+}
+
+//telling our app to start listening for visitors on a the port 3000
+app.listen(3001)
